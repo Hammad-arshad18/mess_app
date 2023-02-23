e72acfc66f5921c81dc3d966b8c41b5d390768c7
Hammad Arshad <hammad.arshad672@gmail.com>
2023-02-23 21:49:04 +0400

    Hammad/Auth-Module Intialize (#1)

---
+++ lib/screens/auth/register.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Register extends StatefulWidget {
+  const Register({super.key});
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
